4bede72a29e78b7f30b166563630fee71cc91435
RIngerslev <101323949+RIngerslev@users.noreply.github.com>
2022-10-06 16:15:35 +0200

    CSS og kontakt

--- countdownTimer.js
+++ countdownTimer.js
@@ -3,8 +3,23 @@ function toggleMenu(){
     const navigation = document.querySelector('.navigation')
     menuToggle.classList.toggle('active')
     navigation.classList.toggle('active')
-    
 };
+
+var btn = document.getElementById('buttonMenu')
+
+btn.addEventListener('click', function(){
+  if(this.classList.contains('active')){
+    const navigation = document.querySelector('.navigation')
+    navigation.classList.toggle('active')
+    this.classList.remove('active')
+    this.classList.add('not-active')
+  }else{
+    const navigation = document.querySelector('.navigation')
+    navigation.classList.toggle('active')
+    this.classList.add('active')
+    this.classList.remove('not-active')
+  }
+});
 
 function changeColor(button){
     button.style.backgroundColor = 'green';
